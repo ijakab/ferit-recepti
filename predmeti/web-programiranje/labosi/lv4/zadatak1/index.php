185d68a1ece6c8ffd101124a26e340e4e7548186
Ivan Jakab <ivan.jakab@gauss.hr>
2020-05-18 18:23:13 +0200

    feat: task 1

--- predmeti/web-programiranje/labosi/lv4/zadatak1/index.php
+++ predmeti/web-programiranje/labosi/lv4/zadatak1/index.php
@@ -4,11 +4,18 @@ echo "Zadatak 1" . "<br /><br />\n";
 
 function getExtension($filepath)
 {
-    /**
-     * Kod zadatka ide ovdje
-     */
+
+
+   if (strpos($filepath, '.') !== false) {
+        $parts = explode('.', $filepath);
+        return $parts[count($parts)-1];
+    }
+    return '';
+
 }
 
 print "blah.gif:      " . getExtension("blah.gif") . "<br />\n";
 print "my.file.gif:   " . getExtension("my.file.gif") . "<br />\n";
 print "this/is/my/new/xfile: " . getExtension("this/is/my/new/xfile") . "<br />\n";
+
+?>
