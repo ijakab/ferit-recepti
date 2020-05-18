20978e2c84c608b7082f893ba1b6bd1b7f0f33ba
Ivan Jakab <ivan.jakab@gauss.hr>
2020-05-18 18:39:35 +0200

    chore: task2 tests

--- predmeti/web-programiranje/labosi/lv4/zadatak2/index.php
+++ predmeti/web-programiranje/labosi/lv4/zadatak2/index.php
@@ -10,5 +10,8 @@ function is_anagram($word1, $word2)
 /**
  * Definirajte dva proizvoljna argumenta unutar funkcije
  */
-$status = is_anagram("example 1", "example 2") ? "Predani parametri su anagrami" : "Predani parametri nisu anagrami";
-echo $status;
+echo is_anagram("example 1", "example 2") ? "Predani parametri su anagrami" : "Predani parametri nisu anagrami";
+echo is_anagram("blaȭtra", "traablȭ") ? "Predani parametri su anagrami" : "Predani parametri nisu anagrami";
+echo is_anagram("hey", "heyȭ") ? "Predani parametri su anagrami" : "Predani parametri nisu anagrami";
+
+?>
