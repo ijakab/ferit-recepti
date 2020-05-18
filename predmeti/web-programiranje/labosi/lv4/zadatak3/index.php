079412fa62638a4b747f3293091980761cb1648c
Ivan Jakab <ivan.jakab@gauss.hr>
2020-05-18 19:04:22 +0200

    feat: task 3

--- predmeti/web-programiranje/labosi/lv4/zadatak3/index.php
+++ predmeti/web-programiranje/labosi/lv4/zadatak3/index.php
@@ -3,5 +3,16 @@
 echo "Zadatak 3" . "<br /><br />\n";
 
 $random = rand(1, 1000);
+$temp = $random;
 
-print $random . " => Rezultat u binarnom formatu: ";
+$bin = "";
+while($temp > 0) {
+    if($temp % 2 == 1) {
+        $bin = 1 . $bin;
+    } else {
+        $bin = 0 . $bin;
+    }
+    $temp = (int) ($temp / 2);
+}
+
+echo $random . " => Rezultat u binarnom formatu: " . $bin;
