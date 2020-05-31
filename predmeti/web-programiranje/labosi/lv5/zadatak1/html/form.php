a9183f49b0e26f3ee4fd98b107bfd38938a074dd
Ivan Jakab <ivan.jakab@gauss.hr>
2020-05-31 10:45:53 +0200

    feat: script to disable form submission

--- predmeti/web-programiranje/labosi/lv5/zadatak1/html/form.php
+++ predmeti/web-programiranje/labosi/lv5/zadatak1/html/form.php
@@ -11,7 +11,7 @@ if($_GET['id']) {
 ?>
 
 <div id="container" class="container">
-    <form action="form_action.php" method="post" enctype="multipart/form-data">
+    <form action="form_action.php" method="post" enctype="multipart/form-data" id="main-form">
         <div class="form-group">
             <label for="name">Name</label>
             <input type="text" class="form-control" id="name" name="name" value="<?php echo $attributes['name'] ?>" required>
@@ -37,14 +37,30 @@ if($_GET['id']) {
                 <div class="col">
                     <label for="age">Image</label>
                     <input type="file" class="form-control" id="image" name="imageHolder" <?php if(!$_GET['id']) { echo 'required'; } ?>>
+                    <!-- This field should only be required when adding new cat, no need to re-upload image if it stays the same -->
                 </div>
             </div>
         </div>
         <div class="form-group">
             <?php if($_GET['id']) { ?>
-                <input type="hidden" name="id" value="<?php echo $_GET['id'] ?>" />
+                <input type="hidden" name="id" id="id_field" value="<?php echo $_GET['id'] ?>" />
             <?php } ?>
+            <!-- Send id to backend if this is edit -->
             <button type="submit" class="btn btn-primary">Submit</button>
         </div>
     </form>
 </div>
+
+<script>
+    window.onload = function () {
+        // this script is actually useless as html5 validation works this out of the box. Just to fill task requirements
+        const fields = ['wins', 'info', 'wins', 'loss', 'age']
+        if(!document.getElementById('id_field')) fields.push('image') // image is only required when adding new, not update
+        const htmlFields = fields.map(field => document.getElementById(field))
+        document.getElementById('main-form').addEventListener('main-form', e => {
+            for(const htmlField of htmlFields) {
+                if(!htmlField.value) e.preventDefault()
+            }
+        })
+    }
+</script>
